2d6f77572cf857acf28b7f309ed4ec0621181111
harjitsingh3 <singh.harjit09@gmail.com>
2023-04-05 12:23:04 -0400

    Finish Importing Data

--- import_db.sql
+++ import_db.sql
@@ -1,9 +1,15 @@
 PRAGMA foreign_keys = ON;
 
+DROP TABLE IF EXISTS question_follows;
+DROP TABLE IF EXISTS question_likes;
+DROP TABLE IF EXISTS replies;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS users;
+
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL,
-    lname TEXT NOT NULL,
+    lname TEXT NOT NULL
 
 );
 
@@ -30,18 +36,18 @@ CREATE TABLE replies (
     question_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
     reply TEXT NOT NULL,
-    child_reply_id INTEGER,
+    parent_id INTEGER,
 
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (question_id) REFERENCES questions(id),
-    FOREIGN KEY (child_reply_id) REFERENCES replies(id)
+    FOREIGN KEY (parent_id) REFERENCES replies(id)
 );
 
 CREATE TABLE question_likes (
     id INTEGER PRIMARY KEY,
     user_id INTEGER NOT NULL,
     question_id INTEGER NOT NULL,
-    like? BOOLEAN,  /* should this be not null? */
+    like_question BOOLEAN,  /* should this be not null? */
 
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (question_id) REFERENCES questions(id)
@@ -53,7 +59,7 @@ INSERT INTO
     users (fname, lname)
 VALUES
     ('Jeremy', 'Liang'),
-    ('Harjit', 'Singh')
+    ('Harjit', 'Singh'),
     ('Kyle', 'Ginzburg');
 
 INSERT INTO
@@ -65,7 +71,12 @@ VALUES
 
 
 INSERT INTO 
-    replies (question_id, user_id, reply, child_reply_id)
+    replies (question_id, user_id, reply, parent_id)
 VALUES
     ((SELECT id FROM questions WHERE title = 'Are you sleeping?'), (SELECT id FROM users WHERE fname = 'Jeremy'), 'Yes.', NULL),
-    ((SELECT id FROM questions WHERE title = 'Are you sleeping?'), (SELECT id FROM users WHERE fname = 'Kyle'), 'Go get some coffee.', (SELECT id FROM replies WHERE reply = 'Yes.'));
+    ((SELECT id FROM questions WHERE title = 'Are you sleeping?'), (SELECT id FROM users WHERE fname = 'Kyle'), 'Go get some coffee.', 1);
+
+INSERT INTO 
+    replies (question_id, user_id, reply, parent_id)
+VALUES
+    ((SELECT id FROM questions WHERE title = 'Are you sleeping?'), (SELECT id FROM users WHERE fname = 'Jeremy'), 'I already had coffee.', (SELECT id FROM replies WHERE id = 1));
